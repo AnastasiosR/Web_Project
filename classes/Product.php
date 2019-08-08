909fd80c14bdb4418d971a53447fb2afc5c1ef64
Anastasios <6208@ait.nsw.edu.au>
2019-08-08 16:17:18 +1000

    2nd Commit

--- classes/Product.php
+++ classes/Product.php
@@ -1,48 +1,33 @@
 <?php
 namespace aitsydney;
+
 use aitsydney\Database;
+
 class Product extends Database{
-    public $products = array();
     public function __construct(){
         parent::__construct();
     }
     public function getProducts(){
-        $query = "SELECT 
-        @product_id := product.product_id AS product_id,
-        product.name,
-        product.description,
-        product.price,
-        ( SELECT @image_id := product_image.image_id FROM product_image WHERE product_image.product_id = @product_id LIMIT 1 ) AS image_id,
-        ( SELECT image_file_name FROM image WHERE image.image_id = @image_id ) AS image
-        FROM product";
-
+        $query = "
+            SELECT 
+            @product_id := product.product_id as product_id,
+            product.name,
+            product.description,
+            product.price,
+            @image_id := ( SELECT image_id FROM product_image WHERE product_id = @product_id LIMIT 1) as image_id,
+            ( SELECT image_file_name FROM image WHERE image_id = @image_id ) as image
+            FROM product 
+        ";
+        
         $statement = $this -> connection -> prepare( $query );
         if( $statement -> execute() ){
             $result = $statement -> get_result();
+            $product_array = array();
             while( $row = $result -> fetch_assoc() ){
-                array_push( $this -> products, $row );
+                array_push( $product_array, $row );
             }
+            return $product_array;
         }
-        return $this -> products;
-    }
-    //get product by id
-    public function getProductDetail($id){
-          $query ="SELECT 
-          product_id,
-          name,
-          description,
-          price
-          FROM product WHERE product_id = ? ";
-
-          $statement = $this -> connection -> prepare ( $query );
-          $statement -> bind_param('i',$id);
-          if($statement ->execute()) {
-              $product_detail = array();
-              $result = $statement -> get_result();
-              $row =$result -> fetch_assoc();
-              $product_detail['product'] = $row;
-              return $product_detail;
-          }
     }
 }
 ?>
--- index.php
+++ index.php
@@ -1,25 +1,24 @@
 <?php
-//php function to call php files from other directories
 require('vendor/autoload.php');
-
 
 use aitsydney\Product;
 
+$products = new Product();
+$products_result = $products -> getProducts();
 
-//create an instance of product class
-$p = new Product();
-$products = $p -> getProducts();
-
-//print_r($products);
-//create twig loader for templates
+//create twig loader
+//$loader = new \Twig\Loader\FilesystemLoader('templates');
 $loader = new Twig_Loader_Filesystem('templates');
+
 //create twig environment
 $twig = new Twig_Environment($loader);
-//call a twig template
+
+//load a twig template
 $template = $twig -> load('home.twig');
-//output the template and pass the data
-echo $template -> render(array(
-    'products' => $products,
-    'title' => 'Welcome to the shop'
-));
+
+//pass values to twig
+echo $template -> render([
+    'products' => $products_result,
+    'title' => 'Hello shop'
+]);
 ?>
